ce26fc61dfa26fbebece66c5074dd1f4db398582
delossantos.eugene <delossantos.eugene@heb.com>
2018-03-28 02:50:32 +0800

    initial working d3 - shuffle and bar graph; Thymeleaf declaration

--- src/main/java/com/delose/datavis/controller/DatavisController.java
+++ src/main/java/com/delose/datavis/controller/DatavisController.java
@@ -1,11 +1,20 @@
 package com.delose.datavis.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.ServletContextAware;
 
 /**
  *
@@ -13,7 +22,10 @@ import org.springframework.web.bind.annotation.RequestParam;
  *
  */
 @Controller
-public class DatavisController {
+public class DatavisController implements ServletContextAware {
+
+    @Autowired
+    ServletContext context;
 
     @GetMapping("/welcome")
     public String welcome(@RequestParam(name="name", required=false, defaultValue="User") String name, Model model) {
@@ -22,9 +34,29 @@ public class DatavisController {
     }
 
     @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
-    public String home(@RequestParam(name="name", required=false, defaultValue="User") String name, Model model) {
+    public String home(@RequestParam(name="name", required=false, defaultValue="User") String name, Model model)
+            throws ServletException, IOException {
         model.addAttribute("name", name);
+//        String d3Path = new DatavisServlet().getRealPath("/vendor/d3/data/data.tsv");
+//        String uploadPath = context.getRealPath("") + File.separator + "/vendor/d3/data/data.tsv";
+//        model.addAttribute("data", uploadPath);
+        List<Integer> array = new ArrayList<Integer>();
+        array.add(100);
+        array.add(200);
+        array.add(300);
+        array.add(400);
+        array.add(100);
+        array.add(200);
+        array.add(300);
+        array.add(400);
+        model.addAttribute("data", array);
         return "dashboard/dashboard";
+    }
+
+    @Override
+    public void setServletContext(ServletContext arg0) {
+        // TODO Auto-generated method stub
+
     }
 
 }
